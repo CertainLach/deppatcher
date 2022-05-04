abb59805228ffac887d4f7fdea61e30751894886
Yaroslav Bolyukin <iam@lach.pw>
2022-05-04 19:20:45 +0300

    feat: rewrite projects instead of workspace
    Signed-off-by: Yaroslav Bolyukin <iam@lach.pw>

---
+++ README.md
@@ -0,0 +1,49 @@
+# deppatcher
+
+Utility for mass rewriting of Cargo.toml files
+
+## Example scenarios
+
+1. You use substrate, you can only depend on git version, and you can't just specify master branch
+
+In repository you have a lot of similar lines:
+```toml
+sp-api = { default-features = false, git = "https://github.com/paritytech/substrate", branch = "polkadot-v0.9.18" }
+```
+When you need to update to next version of this dependency, you search&replace a lot of times, this is annoying, and doesn't always work (I.e if you want to have your own versioning in your forks)
+```toml
+sp-api = { default-features = false, git = "https://github.com/paritytech/substrate", branch = "polkadot-v0.9.19" }
+```
+
+With deppatcher you should just write update rule, like so:
+```jsonnet
+function(pkg) if pkg.source.git == "https://github.com/paritytech/substrate" then pkg.source {
+	branch: "polkadot-v0.9.19"
+}
+```
+
+deppatcher stores original versions of packages for revert, you need to run `deppatcher freeze` to remove them
+
+2. You depend on https://github.com/paritytech/frontier, this repo has a lot of modules, you need to temporary use local fork.
+
+Using `[patch]` and/or manually specifying `path` will work, however, those can only point to virtual manifests, you can't type
+```toml
+fp-consensus = { path = "~/my-frontier-fork" }
+```
+You should type path to dependency in this repository clone
+```toml
+fp-consensus = { path = "~/my-frontier-fork/primitives/consensus" }
+```
+
+deppatcher comes to rescue!
+```jsonnet
+local frontier = dpp.loadPaths('/home/lach/work/substrate/frontier');
+function(pkg) if std.objectHas(frontier, pkg.package) then {
+	path: frontier[pkg.package],
+}
+```
+
+When you need to switch everything back - use `deppatcher revert` command
+
+## Alternatives
+https://github.com/bkchr/diener - very limited, you can't update non-substrate dependency (i.e frontier), revert part of patch, or perform any other non-trivial operation. Everything you can do with diener - you also can do with deppatcher
--- src/main.rs
+++ src/main.rs
@@ -1,181 +1,381 @@
 use std::{
-    collections::{BTreeMap, BTreeSet, HashSet},
+    env::current_dir,
     fs,
-    path::PathBuf,
-    process::Command,
+    io::{stdin, Read},
+    path::{Path, PathBuf},
+    result,
 };
 
-use cargo_lock::{Lockfile, Package};
+use clap::Parser;
+use jrsonnet_cli::{ConfigureState, GeneralOpts, InputOpts};
 use jrsonnet_evaluator::{
     error::{Error, Result},
+    function::{builtin, FuncVal},
+    throw_runtime,
     typed::Either2,
     typed::{Null, Typed},
-    Either, State,
+    Either, ObjValue, ObjValueBuilder, State, Val,
 };
-use toml_edit::{Document, InlineTable, Item, Table, Value};
+use toml_edit::{Document, InlineTable, Item, Table, TableLike, Value};
 use tracing::info;
 
-#[derive(Typed, Clone, PartialEq, Eq, PartialOrd, Ord, Debug)]
-struct PackageDescription {
-    name: String,
-    source: String,
+trait ToRuntime<T> {
+    fn run_err(self) -> Result<T>;
+}
+impl<T, E> ToRuntime<T> for result::Result<T, E>
+where
+    E: ToString,
+{
+    fn run_err(self) -> Result<T> {
+        Ok(self.map_err(|e| Error::RuntimeError(e.to_string().into()))?)
+    }
 }
 
-#[derive(Typed, Debug)]
-struct SourceDescription {
+#[derive(Typed, Debug, Clone, PartialEq, Eq)]
+struct DirectSource {
+    version: Option<String>,
+
     path: Option<String>,
+
     git: Option<String>,
+    rev: Option<String>,
+    tag: Option<String>,
     branch: Option<String>,
 
-    #[typed(rename = "goDeep")]
-    go_deep: Option<bool>,
+    registry: Option<String>,
+}
+impl DirectSource {
+    fn read(table: &dyn TableLike) -> Self {
+        let get = |s: &str| table.get(s).and_then(Item::as_str).map(ToOwned::to_owned);
+        Self {
+            version: get("version"),
+            path: get("path"),
+            git: get("git"),
+            rev: get("rev"),
+            tag: get("tag"),
+            branch: get("branch"),
+            registry: get("registry"),
+        }
+    }
+    fn write(&self, table: &mut dyn TableLike) {
+        let mut set = |s: &str, v: &Option<String>| {
+            if let Some(v) = v {
+                table.insert(s, Item::Value(v.into()));
+            } else {
+                table.remove(s);
+            }
+        };
+        set("version", &self.version);
+        set("path", &self.path);
+        set("git", &self.git);
+        set("rev", &self.rev);
+        set("tag", &self.tag);
+        set("branch", &self.branch);
+        set("registry", &self.registry);
+    }
+    fn to_table(&self) -> InlineTable {
+        let mut table = InlineTable::new();
+        self.write(&mut table);
+        table
+    }
 }
 
-fn find_packages_by_source(
+#[derive(Typed, Debug, Clone)]
+struct DirectInput {
+    name: String,
+    package: String,
+    source: DirectSource,
+    #[typed(rename = "originalSource")]
+    original_source: DirectSource,
+}
+
+type Key = Vec<String>;
+
+type Mutator = dyn Fn(State, DirectInput) -> Result<Either![Null, DirectSource]>;
+
+fn patch_dep(
     s: State,
-    lock: &Lockfile,
-    pkg: &Package,
-    out: &mut BTreeMap<PackageDescription, SourceDescription>,
-    visited: &mut BTreeSet<PackageDescription>,
-    mapper: &impl Fn(State, PackageDescription) -> Result<Either![Null, SourceDescription]>,
+    originals: &mut Item,
+    key: &mut Key,
+    dep: &mut dyn TableLike,
+    mutator: &Mutator,
 ) -> Result<()> {
-    let desc = PackageDescription {
-        name: pkg.name.to_string(),
-        source: pkg.source.as_ref().map_or_else(
-            || "".to_owned(),
-            |s| {
-                if s.is_default_registry() {
-                    "crates-io".to_string()
-                } else {
-                    s.display_registry_name()
-                }
-            },
-        ),
+    let name = key.iter().last().unwrap().as_str();
+    let package = dep
+        .get("package")
+        .and_then(|v| v.as_str())
+        .unwrap_or(name)
+        .to_owned();
+    let source = DirectSource::read(dep);
+    let (had_original, original_source) = get_item(&originals, key.iter().map(String::as_str))
+        .and_then(|i| i.as_table_like())
+        .map(DirectSource::read)
+        .map(|v| (true, v))
+        .unwrap_or_else(|| (false, source.clone()));
+
+    let input = DirectInput {
+        name: name.to_owned(),
+        package: package.clone(),
+        source: source.clone(),
+        original_source: original_source.clone(),
     };
-    if visited.contains(&desc) {
+    let new_source = if let Either2::B(new_source) = mutator(s, input.clone())? {
+        new_source
+    } else {
+        return Ok(());
+    };
+
+    if new_source == source {
         return Ok(());
     }
-    if let Either2::B(source) = mapper(s.clone(), desc.clone())? {
-        let go_deep = source.go_deep.unwrap_or(false);
-        out.insert(desc.clone(), source);
-        if !go_deep {
-            visited.insert(desc);
-            return Ok(());
-        }
-    }
-    visited.insert(desc);
 
-    for dependency in &pkg.dependencies {
-        for pkg in lock.packages.iter().filter(|pkg| dependency.matches(pkg)) {
-            find_packages_by_source(s.clone(), lock, pkg, out, visited, mapper)?;
-        }
+    info!("rewrite {} => {}", source.to_table(), new_source.to_table());
+
+    let originals = originals.as_table_mut().expect("is table checked");
+    if !had_original {
+        let name = key.pop().unwrap();
+        key.push(package);
+        set_table(
+            originals,
+            key,
+            Item::Value(Value::InlineTable(source.to_table())),
+        );
+        key.pop();
+        key.push(name);
+    } else if original_source == new_source {
+        set_table(originals, key, Item::None);
     }
+
+    new_source.write(dep);
 
     Ok(())
 }
 
-fn to_runtime(err: impl ToString) -> Error {
-    Error::RuntimeError(err.to_string().into())
+fn patch_dep_table(
+    s: State,
+    originals: &mut Item,
+    key: &mut Key,
+    deps: &mut Table,
+    mutator: &Mutator,
+) -> Result<()> {
+    for (d, table) in deps
+        .iter_mut()
+        .flat_map(|(k, t)| t.as_table_like_mut().map(|t| (k, t)))
+    {
+        key.push(d.get().to_owned());
+        patch_dep(s.clone(), originals, key, table, mutator)?;
+        key.pop();
+    }
+    Ok(())
+}
+
+fn patch_target_table(
+    s: State,
+    originals: &mut Item,
+    key: &mut Key,
+    target: &mut Table,
+    mutator: &Mutator,
+) -> Result<()> {
+    for kind in ["dependencies", "dev-dependencies", "build-dependencies"] {
+        if let Some(deps) = target.get_mut(kind).and_then(|t| t.as_table_mut()) {
+            key.push(kind.to_owned());
+            patch_dep_table(s.clone(), originals, key, deps, mutator)?;
+            key.pop();
+        }
+    }
+    Ok(())
+}
+
+fn get_item<'t, 'k>(table: &'t Item, key: impl IntoIterator<Item = &'k str>) -> Option<&'t Item> {
+    key.into_iter()
+        .try_fold(table, |table, key| table.as_table_like()?.get(key))
+}
+fn set_table<'t, 'k>(mut table: &mut Table, key: &Key, value: Item) {
+    let (last, path) = key.split_last().unwrap();
+
+    for frag in path {
+        table = if table.contains_table(&frag) {
+            let old = table
+                .get_mut(&frag)
+                .expect("just tested")
+                .as_table_mut()
+                .expect("just tested");
+            old.set_implicit(true);
+            old
+        } else {
+            let mut new = Table::new();
+            new.set_implicit(true);
+            table.insert(&frag, Item::Table(new));
+            table
+                .get_mut(frag)
+                .expect("just added")
+                .as_table_mut()
+                .expect("just added")
+        }
+    }
+    table.insert(&last, value);
+}
+
+fn freeze(path: &Path) -> Result<()> {
+    let toml = fs::read_to_string(path).run_err()?;
+    let mut doc: Document = toml.parse().run_err()?;
+    set_table(
+        doc.as_table_mut(),
+        &vec![
+            "package".to_owned(),
+            "metadata".to_owned(),
+            "deppatcher".to_owned(),
+            "originals".to_owned(),
+        ],
+        Item::None,
+    );
+    let toml = doc.to_string();
+    fs::write(path, toml).run_err()?;
+    Ok(())
+}
+
+fn patch(s: State, path: &Path, mutator: &Mutator) -> Result<()> {
+    let toml = fs::read_to_string(path).run_err()?;
+    let mut doc: Document = toml.parse().run_err()?;
+    let mut originals = get_item(
+        doc.as_item(),
+        ["package", "metadata", "deppatcher", "originals"],
+    )
+    .cloned()
+    .unwrap_or_else(|| {
+        let mut table = Table::new();
+        table.set_implicit(true);
+        Item::Table(table)
+    });
+
+    if !originals.is_table() {
+        throw_runtime!("originals should be table");
+    }
+
+    let table = doc.as_table_mut();
+
+    let mut key = Vec::new();
+    patch_target_table(s.clone(), &mut originals, &mut key, table, mutator)?;
+    if let Some(table) = table.get_mut("target").and_then(|t| t.as_table_mut()) {
+        key.push("target".to_owned());
+        for (k, table) in table
+            .iter_mut()
+            .flat_map(|(k, t)| t.as_table_mut().map(|t| (k, t)))
+        {
+            key.push(k.get().to_owned());
+            patch_target_table(s.clone(), &mut originals, &mut key, table, mutator)?;
+            key.pop();
+        }
+        key.pop();
+    }
+
+    set_table(
+        table,
+        &vec![
+            "package".to_owned(),
+            "metadata".to_owned(),
+            "deppatcher".to_owned(),
+            "originals".to_owned(),
+        ],
+        originals,
+    );
+
+    let toml = doc.to_string();
+    fs::write(path, toml).run_err()?;
+
+    Ok(())
+}
+
+/// Mass rewriter of Cargo.toml files
+#[derive(Parser)]
+#[clap(author)]
+enum Opts {
+    /// Rewrite package sources using specified rule
+    Patch {
+        #[clap(flatten)]
+        input: InputOpts,
+        #[clap(flatten)]
+        general: GeneralOpts,
+    },
+    /// Revert back to original packages version, alias to `patch -e 'function(p) p.originalSource'`
+    Revert,
+    /// Remove all saved original packages
+    Freeze,
+}
+
+#[builtin]
+fn load_paths(s: State, tree: String) -> Result<ObjValue> {
+    let tree = PathBuf::from(tree);
+    let mut command = cargo_metadata::MetadataCommand::new();
+    command.no_deps();
+    command.current_dir(tree);
+    let metadata = command.exec().run_err()?;
+
+    let mut out = ObjValueBuilder::new();
+    for package in &metadata.packages {
+        let path = package.manifest_path.parent().unwrap();
+        out.member(package.name.clone().into())
+            .value(s.clone(), Val::Str(path.to_string().into()))?;
+    }
+    Ok(out.build())
 }
 
 fn main() -> Result<()> {
     tracing_subscriber::fmt::init();
 
-    // {
-    //     info!("removing patch section");
-    //     let toml = fs::read_to_string("./Cargo.toml").map_err(to_runtime)?;
-
-    //     let mut document = toml.parse::<Document>().map_err(to_runtime)?;
-    //     let table = document.as_table_mut();
-
-    //     table.remove("patch");
-
-    //     fs::write("./Cargo.toml", document.to_string()).map_err(to_runtime)?;
-    // }
-
-    let mut checked_lockfiles: HashSet<String> = HashSet::new();
-
-    let mut patches = BTreeMap::new();
-    loop {
-        {
-            info!("regenerating lockfile");
-            let _lock = Command::new("cargo")
-                .arg("generate-lockfile")
-                .status()
-                .map_err(to_runtime)?;
-        }
-        let lock = Lockfile::load("./Cargo.lock").map_err(to_runtime)?;
-        if checked_lockfiles.contains(&lock.to_string()) {
-            info!("lockfile remaint same");
-            break;
-        }
-
-        {
-            info!("unique sources:");
-            let mut out = BTreeSet::new();
-            for p in &lock.packages {
-                out.insert(
-                    p.source
-                        .as_ref()
-                        .cloned()
-                        .unwrap_or_default()
-                        .display_registry_name(),
-                );
-            }
-            for v in out {
-                info!("- {}", v);
+    let mut opts = Opts::parse();
+    if let Opts::Revert = opts {
+        opts = Opts::parse_from(["deppatcher", "patch", "-e", "function(p) p.originalSource"])
+    };
+    match opts {
+        Opts::Freeze => {
+            for entry in walkdir::WalkDir::new(current_dir().run_err()?) {
+                let entry = entry.run_err()?;
+                if entry.file_type().is_file() && entry.path().ends_with("Cargo.toml") {
+                    info!("freezing {}", entry.path().display());
+                    freeze(&entry.path())?;
+                }
             }
         }
+        Opts::Revert => unreachable!("this is alias"),
+        Opts::Patch { input, general } => {
+            let s = State::default();
 
-        info!("generating new patches");
-        let s = State::default();
-        s.set_import_resolver(Box::new(jrsonnet_evaluator::FileImportResolver::default()));
-        let f = s.evaluate_file_raw(&PathBuf::from("./Cargo.dpp"))?;
+            let mut dpp = ObjValueBuilder::new();
+            dpp.member("loadPaths".into()).value(
+                s.clone(),
+                Val::Func(FuncVal::StaticBuiltin(load_paths::INST)),
+            )?;
+            let dpp = dpp.build();
 
-        let f = f.as_func().unwrap();
-        let mapper = f.into_native::<((PackageDescription,), Either![Null, SourceDescription])>();
-
-        let mut visited = BTreeSet::new();
-        for package in lock.packages.iter().filter(|p| p.source.is_none()) {
-            find_packages_by_source(s.clone(), &lock, package, &mut patches, &mut visited, &mapper)?;
-        }
-
-        {
-            info!("writing it to patch section");
-            let toml = fs::read_to_string("./Cargo.toml").map_err(to_runtime)?;
-
-            let mut document = toml.parse::<Document>().map_err(to_runtime)?;
-            let table = document.as_table_mut();
-
-            if !table.contains_table("patch") {
-                table.insert("patch", Item::Table(Table::new()));
-            }
-            let patch = table.get_mut("patch").unwrap().as_table_mut().unwrap();
-            patch.set_implicit(true);
-
-            for (pkg, new_source) in &patches {
-                if !patch.contains_table(&pkg.source) {
-                    patch.insert(&pkg.source, Item::Table(Table::new()));
-                }
-                let source = patch.get_mut(&pkg.source).unwrap().as_table_mut().unwrap();
-                let mut override_source = InlineTable::new();
-
-                if let Some(git) = &new_source.git {
-                    override_source.insert("git", git.into());
-                }
-                if let Some(branch) = &new_source.branch {
-                    override_source.insert("branch", branch.into());
-                }
-                if let Some(path) = &new_source.path {
-                    override_source.insert("path", path.into());
-                }
-
-                source.insert(&pkg.name, Item::Value(Value::InlineTable(override_source)));
+            {
+                s.settings_mut()
+                    .globals
+                    .insert("dpp".into(), Val::Obj(dpp.clone()));
             }
 
-            fs::write("./Cargo.toml", document.to_string()).map_err(to_runtime)?;
+            general.configure(&s)?;
+
+            let mutator = if input.exec {
+                s.evaluate_snippet_raw(PathBuf::from("<cmdline>").into(), input.input.into())?
+            } else if input.input.as_str() == "-" {
+                let mut code = String::new();
+                stdin().read_to_string(&mut code).run_err()?;
+                s.evaluate_snippet_raw(PathBuf::from("<stdin>").into(), code.into())?
+            } else {
+                s.evaluate_file_raw(&PathBuf::from(input.input))?
+            };
+            let mutator = FuncVal::from_untyped(mutator, s.clone())?;
+            let mutator = mutator.into_native::<((DirectInput,), Either![Null, DirectSource])>();
+
+            for entry in walkdir::WalkDir::new(current_dir().run_err()?) {
+                let entry = entry.run_err()?;
+                if entry.file_type().is_file() && entry.path().ends_with("Cargo.toml") {
+                    info!("patching {}", entry.path().display());
+                    patch(s.clone(), &entry.path(), &mutator)?;
+                }
+            }
         }
-        checked_lockfiles.insert(lock.to_string());
     }
 
     Ok(())
